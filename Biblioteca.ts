d6945f931ebd6c47e1dc167888b12a37c0a6531a
EdivanMayolo <edivanmayolo98@gmail.com>
2025-11-01 17:05:41 -0300

    Ajustes Finais

--- Biblioteca.ts
+++ Biblioteca.ts
@@ -39,7 +39,7 @@ public emprestar(membro: Membro): void {
         this.membroUsando = membro;
         console.log(`O livro "${this.titulo}" é seu ${membro.nome}... Até Vc Devolver.`);
     } else {
-        console.log(`Livro "${this.titulo}"já está emprestado.`);
+        console.log(`Livro "${this.titulo}" já está emprestado.`);
     }
   }
 //Validação para devolver o livro usando IF (!this.disonibilidade)
@@ -51,7 +51,7 @@ public devolver(): void {
         this.membroUsando.livrosEmprestados = 
             this.membroUsando.livrosEmprestados.filter(livro => livro !== this);
         // this.membroUsando = undefined!;
-        console.log(`Livro Devolvido "${this.titulo} Obrigado ${this.membroUsando.nome}!!".`);
+        console.log(`Livro "${this.titulo}" devolvido, Obrigado "${this.membroUsando.nome}!!".`);
         this.membroUsando = undefined!;
     } else {
         console.log(`Nao é possivel devolver "${this.titulo}" pois não esta emprestado.`);
@@ -97,36 +97,43 @@ const membro3 = new Membro("Leide Daiane", 56789);
 const livro1 = new Livro("Dom Casmurro", "Machado de Assis", "Principis", 1899);
 const livro2 = new Livro("Casmurro", "Assis", "Principis", 1899);
 const livro3 = new Livro("Companhia das Letras", "George Orwell", "Papel" , 1949);
-const livro4 = new Livro( "O mundo é uma bola de Golfe:", "Autor Desconhecido","Matrix", 1949);
+const livro4 = new Livro( "O mundo é uma bola de Golfe?", "Autor Desconhecido","Matrix", 1949);
 //BLOCO PARA TESTE.
-console.log("\n=== Objetos instanciados com sucesso ===");
+/*console.log("\n=== Objetos instanciados com sucesso ===\n");
 console.log(livro1, livro2, livro3, livro4);
-console.log(membro1, membro2, membro3);
+console.log(membro1, membro2, membro3);*/
 
-//Validações do devolver e do emprestar
+//Validações dos métodos devolver e do emprestar
+// ========================= Teste 1 =========================
 
-//Teste 1
-/*livro1.devolver();//MSG que não está emprestado
-livro2.devolver();//MSG que não está emprestado
-livro3.devolver();//MSG que não está emprestado
-livro4.devolver();*///MSG que não está emprestado
+// console.log("\n*** Teste 1: Devolver livros que não estão emprestados ***\n");
+// livro1.devolver();//MSG que não está emprestado
+// livro2.devolver();//MSG que não está emprestado
+// livro3.devolver();//MSG que não está emprestado
+// livro4.devolver();///MSG que não está emprestado
 
-//Teste 2
-/*livro1.emprestar(membro1);
-livro2.emprestar(membro2);
-livro3.emprestar(membro2);
-livro4.emprestar(membro2);
-livro1.emprestar(membro1);*/
+// ========================= Teste 2 =========================
 
-//Teste 3
-/*livro1.emprestar(membro2);//João Pedro pega Dom Casmurro
+// console.log("\n*** Teste 2: Emprestar livros disponíveis ***\n");
+// livro1.emprestar(membro1);
+// livro2.emprestar(membro2);
+// livro3.emprestar(membro2);
+// livro4.emprestar(membro2);
+// livro1.emprestar(membro1);
+
+// ========================= Teste 3 =========================
+/*
+console.log("\n*** Teste 3: Emprestar livros indisponíveis e devolver livros ***\n");
+livro1.emprestar(membro2);//João Pedro pega Dom Casmurro
 livro1.emprestar(membro3) //já está emprestado
 livro4.emprestar(membro2);//João Pedro pega O mundo é uma bola de Golfe:
 livro1.devolver();//faz a devolução
 livro1.devolver();*///MSG que não está emprestado
 
-//Teste 4
-/*livro4.emprestar(membro2)
+// ========================= Teste 4 =========================
+/*
+console.log("\n*** Teste 4: Emprestar e devolver livros com várias operações ***\n");
+livro4.emprestar(membro2)
 livro4.devolver();
 livro4.emprestar(membro1);
 livro4.devolver();
@@ -137,14 +144,92 @@ console.log(membro3)
 console.log(membro1)
 console.log(membro2)*/
 
-//Teste 5
-/*livro3.devolver();//MSG que não está emprestado
+// ========================= Teste 5 =========================
+/*
+console.log("\n*** Teste 5: Devolver livros e tentar emprestar novamente ***\n");
+livro3.devolver();//MSG que não está emprestado
 livro3.emprestar(membro2);//Edivan Mayolo pega Companhia das Letras
 livro3.emprestar(membro3);//MSG que já está emprestado
 livro2.devolver();//MSG que não está emprestado
 livro3.emprestar(membro3);//MSG que já está emprestado
 console.log(membro2)
 console.log(membro3)*/
-//membro1.pegarLivrosEmprestados(livro1);
-//membro1.devolverLivrosEmprestados(livro2);
 
+// ========================= Teste 6 =========================
+// console.log("\n*** Teste 7: Devolver livros e tentar emprestar novamente ***\n");
+// livro1.emprestar(membro1);
+// livro3.emprestar(membro1);
+// livro1.emprestar(membro2);
+// livro1.emprestar(membro1);
+// livro1.devolver();
+// livro1.emprestar(membro2);
+// livro1.emprestar(membro3);
+// livro2.emprestar(membro3);
+
+// console.log({
+//   Edivan: membro1.livrosEmprestados.map(l => l.titulo),
+//   Joao:   membro2.livrosEmprestados.map(l => l.titulo), 
+//   Leide:  membro3.livrosEmprestados.map(l => l.titulo)  
+// });
+// ====================== Teste 7 =========================
+//Garantir que os livros estão disponíveis
+// console.log("\n=== Teste  ===");
+// livro1.devolver();
+// livro2.devolver();
+// livro3.devolver();
+// livro4.devolver();
+
+// membro1.pegarLivrosEmprestados(livro1); // deve emprestar normalmente
+// membro1.pegarLivrosEmprestados(livro1); // deve avisar que já possui o livro
+// console.log("Livros de Edivan:", membro1.livrosEmprestados.map(l => l.titulo));
+
+// ====================== Teste 8 =========================
+// console.log("\n=== Teste 8 ===\n");
+// livro2.devolver(); //Garantir que os livros estão disponíveis//
+// membro2.pegarLivrosEmprestados(livro2); // João pega
+// membro3.pegarLivrosEmprestados(livro2); // Leide tenta, deve receber mensagem de indisponível
+// membro2.devolverLivrosEmprestados(livro2); // João devolve
+// membro3.pegarLivrosEmprestados(livro2); // Leide pega agora
+
+// console.log({
+//   Joao: membro2.livrosEmprestados.map(l => l.titulo),
+//   Leide: membro3.livrosEmprestados.map(l => l.titulo)
+// });
+// ====================== Teste 9 =========================
+// console.log("\n=== Teste 9 ===\n");
+// console.log("Garantir que os livros estão disponíveis\n");
+// livro1.devolver(); 
+// livro3.devolver(); 
+// livro4.devolver(); 
+// console.log("\n");
+// membro1.pegarLivrosEmprestados(livro1); // Edivan pega 1
+// membro1.pegarLivrosEmprestados(livro3); // Edivan pega 3
+// membro1.pegarLivrosEmprestados(livro4); // Edivan pega 4
+// console.log("\n");
+// console.log("Antes de Devolver:", membro1.livrosEmprestados.map(l => l.titulo));
+// console.log("\n");
+// membro1.devolverLivrosEmprestados(livro3); // devolve um
+// membro1.devolverLivrosEmprestados(livro1); // devolve outro
+// membro1.devolverLivrosEmprestados(livro4); // devolve o último
+// console.log("\n");
+// console.log("Depois de Devolver:", membro1.livrosEmprestados.map(l => l.titulo));
+
+// ====================== Teste 10 =========================
+ console.log("\n=== Teste 10 ===\n");
+
+membro3.pegarLivrosEmprestados(livro4); // Leide pega
+console.log("\n");
+membro2.devolverLivrosEmprestados(livro3); // João tenta devolver, mas não tem
+console.log("\n");
+membro2.pegarLivrosEmprestados(livro4); // João tenta pegar, mas não pode
+console.log("\n");
+membro1.devolverLivrosEmprestados(livro2); // Edivan tenta devolver, mas não tem
+console.log("\n");
+membro2.pegarLivrosEmprestados(livro3); // João pega
+console.log("\n");
+membro3.pegarLivrosEmprestados(livro4); // Leide tenta pegar de novo   
+console.log({
+  Edivan: membro1.livrosEmprestados.map(l => l.titulo),
+  Joao: membro2.livrosEmprestados.map(l => l.titulo),
+  Leide: membro3.livrosEmprestados.map(l => l.titulo)
+});
