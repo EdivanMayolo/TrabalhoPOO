da8f8ff005969487adab4660ef5828e067df401c
EdivanMayolo <edivanmayolo98@gmail.com>
2025-10-31 21:11:52 -0300

    Criado Objeto Membro

--- Biblioteca.ts
+++ Biblioteca.ts
@@ -1,13 +1,15 @@
+ /*Bloco de declaração do molde/receita
+    enquanto os atribitos são os ingredientes do molde/receita    
+*/
 class Livro{
     titulo : string;
     autor : string;
     editora : string;
     anoPublicacao : number;
-    disonibilidade : boolean;
-    /*Bloco de declaração do molde/receita
-    enquanto os atribitos são os ingredientes do molde/receita    
-    */
+    disonibilidade : boolean;  
 
+    /* O onstrutor criado é método especial chamado automaticamente quando se usarmos new Livro(...) por exemplo e serve para inicializar os atributos do objeto acima
+    */
     constructor (
         titulo : string,
         autor : string,
@@ -15,28 +17,24 @@ class Livro{
         anoPublicacao : number,
         disonibilidade : boolean = true
     )
-    /* O onstrutor criado é método especial chamado automaticamente quando se usarmos new Livro(...) por exemplo e serve para inicializar os atributos do objeto acima
-    */
     {
        this.titulo = titulo;
        this.autor = autor;
        this.editora = editora;
        this.anoPublicacao = anoPublicacao;
        this.disonibilidade = disonibilidade; 
-    }/*
-    this.atributo refere-se ao atributo do objeto que está sendo criado
-    atributo refere-se ao parâmetro passado para o construtor
-    */
-    
+    }
 
+//Validação de disponibilidade do livro atraves de IF    
 emprestar(): void {
     if (this.disonibilidade) {
         this.disonibilidade = false;
-        console.log(`O livro "${this.titulo}" é seu "Até Vc Devolver ".`);
+        console.log(`O livro "${this.titulo}" é seu... Até Vc Devolver.`);
     } else {
         console.log(`Livro "${this.titulo}"já está emprestado.`);
     }
   }
+//Validação para devolver o livro usando IF (!this.disonibilidade)
 devolver(): void {
     if (!this.disonibilidade) {
         this.disonibilidade = true;
@@ -46,20 +44,40 @@ devolver(): void {
         }
     }
 }
+
+/*
+BLOCO PARA TESTE.
+*/
 /*const livro1 = new Livro("Dom Casmurro", "Machado de Assis", "Principis", 1899, true);
 console.log(livro1);
 //console.log(***********)
 const livro2 = new Livro("1984", "George Orwell", "Companhia das Letras", 1949, false);
 console.log(livro2.titulo); 
 console.log(livro2.autor);
-console.log(livro2.disonibilidade); 
-/*
-BLOCO PARA TESTE.
-*/
+console.log(livro2.disonibilidade); */
+
 const livro1 = new Livro("Dom Casmurro", "Machado de Assis", "Principis", 1899);
 
 
-livro1.emprestar(); // Empresta com sucesso
+/*livro1.emprestar(); // Empresta com sucesso
 livro1.emprestar(); // Tenta emprestar de novo (erro lógico)
 livro1.devolver();  // Devolve com sucesso
-livro1.devolver();  // Tenta devolver novamente (aviso)
+livro1.devolver();  // Tenta devolver novamente (aviso)*/
+
+class Membro{
+    nome : string;
+    identificacao : number;
+    livrosEmprestados : Livro[];
+
+    constructor (
+        nome : string,
+        identificacao : number
+    )
+    {
+       this.nome = nome;
+       this.identificacao = identificacao;
+       this.livrosEmprestados = []; 
+    }
+}
+const membro1 = new Membro("Edivan Mayolo", 12345);
+console.log(membro1);
