a9fa6681c2296d9dad4bce3cc3ea7d8e68e4a665
EdivanMayolo <edivanmayolo98@gmail.com>
2025-11-08 19:11:57 -0300

    Implementado método Transferir e add comentáarios

--- Banco.ts
+++ Banco.ts
@@ -1,5 +1,6 @@
-//Classe usando Encapsulamento
-class ContaBanco{
+//Classe usando Encapsulamento e Abstração
+//Com atributos privados
+class ContaBancaria{
     private saldo: number; 
     private historico: string[] = [];
         
@@ -12,6 +13,18 @@ class ContaBanco{
             this.historico = [];
         }
 //Métodos usando Abstração
+
+    public transferir(valor: number): void{
+        //validação para evitar valores negativos ou nulos
+        if (valor<= 0){
+            throw new Error("Valor deve ser maior que 0.")};
+
+        this.saldo += valor;
+        //const para capturar a data
+        const data = new Date().toLocaleDateString("pt-BR")
+        //Coloca uma descrição no Histórico
+        this.historico.push(`[${data}]Transferencia de R$ ${valor.toFixed(2)}`); 
+    }
     public Saldo(): number {
         return this.saldo;
     }
@@ -19,7 +32,7 @@ class ContaBanco{
     public Historico(): string[] {
         return this.historico;
     }
-
+    //métodos privados de Dados da conta
     private NumeroConta(): number {
         return this.numeroConta;
     }
